524c2f612c9f6ffc62bf46df425bd4afff2ca24e
Ben Mangold <1518509+benmangold@users.noreply.github.com>
2020-09-27 20:29:54 -0400

    (feat) update dev-server-role to v0.0.4 (#7)

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ terraform {
     }
 }
 
-
 variable "server_port" {
     description = "The port used by the server for HTTP requests"
     type = number
@@ -33,9 +32,8 @@ data "aws_subnet_ids" "default" {
     vpc_id = data.aws_vpc.default.id
 }
 
-
 resource "aws_launch_configuration" "example" {
-    image_id = "ami-03b3817c5ba904afe"
+    image_id = "ami-023e0f930dc46bc05"
     instance_type = "t2.medium"
     key_name = var.key_name
     security_groups = [aws_security_group.instance.id]
